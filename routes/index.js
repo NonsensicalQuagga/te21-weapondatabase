a79091424c529edd3cf6b490578068c251bd7247
NonsensicalQuagga <alvin.hogdal@elev.ga.ntig.se>
2024-02-15 11:17:19 +0100

    Added template for a new attack + an example file

--- routes/index.js
+++ routes/index.js
@@ -16,11 +16,11 @@ router.get('/dbtest/:id', async (req, res) =>{
   try {
     const id = req.params.id
     const [weapon] = await pool.promise().query(`   
-    SELECT alvin_type.name, alvin_type.description, alvin_attacks.input, alvin_attacks.description AS attack_description, alvin_attacks.mv
-    FROM alvin_weapon
-    JOIN alvin_type ON alvin_weapon.weapon_type = alvin_type.id
-    JOIN alvin_attacks on alvin_weapon.Weapon_moveset = alvin_attacks.moveset
-    WHERE alvin_weapon.id = ${id}
+      SELECT alvin_type.name, alvin_type.description, alvin_attacks.input, alvin_attacks.description AS attack_description, alvin_attacks.mv
+      FROM alvin_weapon
+      JOIN alvin_type ON alvin_weapon.type = alvin_type.id
+      JOIN alvin_attacks on alvin_weapon.moveset = alvin_attacks.moveset
+     WHERE alvin_weapon.id = ${id}
     `)
     // const [options] =  await pool.promise().query(`SELECT * FROM alvin_option WHERE part_id = ${id}`)
     const temp = weapon[0];
@@ -38,7 +38,49 @@ router.get('/dbtest/:id', async (req, res) =>{
   }
 })
 
+router.get('/new_attack', async function (req, res) {
+  try {
+    const [weapon] = await pool.promise().query(`
+      SELECT alvin_type.name, alvin_weapon.moveset
+      FROM alvin_weapon
+      JOIN alvin_type ON alvin_weapon.type = alvin_type.id
+    `)
+    console.log(weapon)
+    return res.render('new_attack.njk', {
+      title: 'new attack',
+      weapons: weapon
+    })
+    
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
 
+router.get('/new_weapon', async function (req, res) {
+  try {
+    const [breed] = await pool.promise().query('SELECT * FROM jens_cat_breed')
+    return res.render('new_weapon.njk', {
+      title: 'Ny katt',
+      breeds: breed
+    })
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+router.post('/new_attack', async function (req, res) {
+  res.json(req.body) //för att kolla och kika den data vi får från front-end
+
+
+})
+
+router.post('/cats', async function (req, res) {
+  res.json(req.body) //för att kolla och kika den data vi får från front-end
+
+
+})
 
 
 module.exports = router
