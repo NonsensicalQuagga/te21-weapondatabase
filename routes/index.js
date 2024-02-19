2abca3ffa08c04a8534e2ce3fb4cafb5eca8a2a9
NonsensicalQuagga <alvin.hogdal@elev.ga.ntig.se>
2024-02-19 12:41:32 +0100

    post data to database

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', function (req, res) {
-  res.render('index.njk')
+router.get('/', async function (req, res) {
+  try {
+ // res.render('index.njk')
+  const [weapon] = await pool.promise().query(`
+  SELECT alvin_type.id, alvin_type.name  FROM alvin_type
+  `)
+  console.log(weapon)
+  return res.render('index.njk', {
+    title: 'Weapons',
+    weapons: weapon
+  })
+} catch(error){
+  console.log(error)
+  res.sendStatus(500)
+}
+
 })
 
 
@@ -71,7 +85,18 @@ router.get('/new_weapon', async function (req, res) {
 })
 
 router.post('/new_attack', async function (req, res) {
-  res.json(req.body) //för att kolla och kika den data vi får från front-end
+ // res.json(req.body) //för att kolla och kika den data vi får från front-end
+  try {
+    const [result] = await pool.promise().query(
+      `INSERT INTO alvin_attacks (input, mv, description, moveset)
+      VALUES (?, ?, ?, ?)`,
+      [req.body.input, req.body.mv, req.body.description, req.body.weapon]
+    )
+    res.redirect('/index.njk')
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
 
 
 })
